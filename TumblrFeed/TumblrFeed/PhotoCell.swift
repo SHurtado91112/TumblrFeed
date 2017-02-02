305f506025acfeee824c6f48021e52f34e1c9df0
SH-Arrow <shurt91112@gmail.com>
2017-02-01 20:06:46 -0500

    finished lab

---
+++ TumblrFeed/TumblrFeed/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  TumblrFeed
+//
+//  Created by Steven Hurtado on 2/1/17.
+//  Copyright © 2017 Steven Hurtado. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
